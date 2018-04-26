915d00342b61cf4e19a0430ab7df67c27abb19ed
River <jkjk822@yahoo.com.au>
2018-04-26 16:46:09 -0400

    Add streetMapParser to taxi parser

--- portugalTaxiParser.py
+++ portugalTaxiParser.py
@@ -1,15 +1,19 @@
+from streetMapParser import buildRoadGraph
+
+def parseTrips(filename):
+	trips = {}
+	with open(filename) as f:
+		f.readline()
+		for line in f:
+			tripID = line.split(",")[0][1:-1] #trim quotes
+			rawTripData = line.split("\",\"")[-1][1:-3] #trim outer brackets
+			if not rawTripData:
+				break
+			rawTripData = rawTripData[1:-1] #trim first and last bracket
+			tripData = [(float(pair.split(",")[0]), float(pair.split(",")[1])) for pair in rawTripData.split("],[")]
+			trips[tripID] = tripData
+	return trips
 
 
-trips = {}
-with open('portugalTaxi.csv') as f:
-	f.readline()
-	for line in f:
-		tripID = line.split(",")[0][1:-1] #trim quotes
-		rawTripData = line.split("\",\"")[-1][1:-3] #trim outer brackets
-		if not rawTripData:
-			continue
-		rawTripData = rawTripData[1:-1] #trim first and last bracket
-		tripData = [(float(pair.split(",")[0]), float(pair.split(",")[1])) for pair in rawTripData.split("],[")]
-		trips[tripID] = tripData
-
-print(len(trips))
+trips = parseTrips('portoTaxi.csv')
+graph, edgeTable = buildRoadGraph('portoMap.xml')
