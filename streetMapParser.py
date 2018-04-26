07dfc38ba41b3995ed02ad70dd8bbf13554433f1
River <jkjk822@yahoo.com.au>
2018-04-26 16:44:56 -0400

    Modularized functionality into functions

--- streetMapParser.py
+++ streetMapParser.py
@@ -1,21 +1,9 @@
 from xml.etree import ElementTree as ET
 import networkx as nx
 
-tree = ET.parse('map')
-root = tree.getroot()
 
-
-graph = nx.MultiDiGraph()
-
-##############
-# Find nodes #
-##############
-# Each node has the form
-#   <node id="1890707217" lat="39.8583920" lon="116.1331899" version="1" timestamp="2012-08-30T22:40:49Z" changeset="12924496" uid="376715" user="R438"/>
-# and we hash it with `id` as they key and only keeping the `lat` and `lon` attributes
-nodes = {}
-for node in root.findall('node'):
-	nodes[node.get('id')] = { key: float(node.get(key)) for key in ['lat','lon'] }
+def buildRoadGraph(filename):
+	return __linkNodes(ET.parse(filename).getroot())
 
 #################
 # Find highways #
@@ -34,38 +22,52 @@ for node in root.findall('node'):
 # oneway, etc.
 # We keep all this information for future use
 
-edges = {}
-for way in root.findall('way'):
-	highway = False
-	for tag in way.findall('tag'):
-		if tag.get('k')=='highway':
-			highway	= True
-			break
-	if highway: # Graph construction (see below)
-		edgeID = way.get('id')
-		edges[edgeID] = {tag.get('k'):tag.get('v') for tag in way.findall('tag')}
-		prev = None
-		for node in way.findall('nd'):
-			nodeID = node.get('ref')
-			graph.add_node(nodeID, **nodes[nodeID])
-			if prev:
-				graph.add_edge(prev, nodeID, key=edgeID)
-				if edges[edgeID].get('oneway', 'no') != 'yes':
-					graph.add_edge(nodeID, prev, key=edgeID)
-			prev = nodeID
+def __linkNodes(root):
+	graph = nx.MultiDiGraph()
+	nodes = __findNodes(root)
+	edges = {}
+	for way in root.findall('way'):
+		highway = False
+		for tag in way.findall('tag'):
+			if tag.get('k')=='highway':
+				highway	= True
+				break
+		if highway:
+			__addToGraph(way, nodes, edges, graph)
+	return (graph, edges)
 
 
-##############################
-# Graph Construction (above) #
-##############################
+##############
+# Find nodes #
+##############
+# Each node has the form
+#   <node id="1890707217" lat="39.8583920" lon="116.1331899" version="1" timestamp="2012-08-30T22:40:49Z" changeset="12924496" uid="376715" user="R438"/>
+# and we hash it with `id` as they key and only keeping the `lat` and `lon` attributes
+def __findNodes(root):
+	nodes = {}
+	for node in root.findall('node'):
+		nodes[node.get('id')] = { key: float(node.get(key)) for key in ['lat','lon'] }
+	return nodes
+
+
+######################
+# Graph Construction #
+######################
 # We add only the nodes which are part of a highway
 #
 # We add edges between each of these nodes, only
 # directly storing their `id`. We store the rest of
 # the meta-data in a separate dict to conserve space.
 
-
-print(len(graph.nodes))
-print(len(edges))
-print(len(graph.edges))
-print(edges)
+def __addToGraph(way, nodes, edges, graph):
+	edgeID = way.get('id')
+	edges[edgeID] = {tag.get('k'):tag.get('v') for tag in way.findall('tag')}
+	prev = None
+	for node in way.findall('nd'):
+		nodeID = node.get('ref')
+		graph.add_node(nodeID, **nodes[nodeID])
+		if prev:
+			graph.add_edge(prev, nodeID, key=edgeID)
+			if edges[edgeID].get('oneway', 'no') != 'yes':
+				graph.add_edge(nodeID, prev, key=edgeID)
+		prev = nodeID
