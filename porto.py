da9bb1f9879e65619357b3e555c216421b1c0237
River <jkjk822@yahoo.com.au>
2018-04-28 18:36:07 -0400

    Main logic to porto.py

---
+++ porto.py
@@ -0,0 +1,48 @@
+# Windows Linux subsystem workarounds
+import matplotlib
+matplotlib.use('Agg')
+# Windows Linux subsystem workarounds
+import matplotlib.pyplot as plt
+import networkx as nx
+import json
+from streetMapParser import buildRoadGraph, snapToGraph
+
+
+graph, edgeTable = buildRoadGraph('portoMap.xml')
+# print(nx.number_weakly_connected_components(graph))
+# print(len(graph.edges))
+# for c in nx.weakly_connected_components(graph):
+# 	print(len(graph.subgraph(c)))
+print("built")
+#Read in trips using:
+trips = {}
+with open("portoSnappedTrips.json", 'r') as f:
+	trips = json.loads(f.read())
+print("loaded")
+for tripID in trips:
+	path = []
+	prev = None
+	for nodeID in trips[tripID]:
+		print(nodeID in graph)
+		if prev:
+			path += nx.bidirectional_dijkstra(graph, prev, nodeID)[1]
+		prev = nodeID
+	print(path)
+	print(nx.bidirectional_dijkstra(graph, trips[tripID][0], trips[tripID][-1])[1])
+	print()
+
+# trips, error = snapToGraph(trips, graph)
+# print("snapped to graph")
+# with open("portoSnappedTrips.json", 'w') as f: # encode for later use
+# 	f.write(json.dumps(trips, indent=4))
+# print("wrote to file")
+# print(error)
+
+
+
+
+#for u, v, keys, dist in graph.edges(data='dist', keys=True):
+#	print(dist)
+# nx.draw(graph)
+# plt.savefig('test.pdf')
+
--- portugalTaxiParser.py
+++ portugalTaxiParser.py
@@ -1,13 +1,4 @@
-from streetMapParser import buildRoadGraph, snapToGraph
-import networkx as nx
 
-# Windows Linux subsystem workarounds
-import matplotlib
-matplotlib.use('Agg')
-# Windows Linux subsystem workarounds
-import matplotlib.pyplot as plt
-
-import json
 
 def parseTrips(filename):
 	trips = {}
@@ -22,24 +13,3 @@ def parseTrips(filename):
 			tripData = [(float(pair.split(",")[1]), float(pair.split(",")[0])) for pair in rawTripData.split("],[")]
 			trips[tripID] = tripData
 	return trips
-
-trips = parseTrips('portoTaxi.csv')
-print("parsed trips")
-graph, edgeTable = buildRoadGraph('portoMap.xml')
-print("graph built")
-trips, error = snapToGraph(trips, graph)
-print("snapped to graph")
-with open("portoSnappedTrips.json", 'w') as f: # encode for later use
-	f.write(json.dumps(trips, indent=4))
-print("wrote to file")
-print(error)
-
-#Read back in using:
-# with open("portoSnappedTrips.json", 'r') as f:
-# 	print(json.loads(f.read()))
-
-
-#for u, v, keys, dist in graph.edges(data='dist', keys=True):
-#	print(dist)
-# nx.draw(graph)
-# plt.savefig('test.pdf')
