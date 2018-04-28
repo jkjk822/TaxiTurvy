86aeeae7579ada63d7403f7611c4bf8306c9f925
River <jkjk822@yahoo.com.au>
2018-04-28 18:35:45 -0400

    Added haversine and speeds

--- streetMapParser.py
+++ streetMapParser.py
@@ -4,7 +4,22 @@ import scipy
 import numpy as np
 
 def buildRoadGraph(filename):
-	return __linkNodes(ET.parse(filename).getroot())
+	speeds = {}
+	if filename.startswith('port'):
+		# median speeds
+		speeds = {'living_street': 50, 'primary_link': 50, 'primary': 50, 'track': 35, 'unclassified': 50,
+		'tertiary_link': 50, 'residential': 50, 'secondary': 50, 'pedestrian': 50, 'trunk': 120, 'tertiary': 50,
+		'service': 30, 'motorway': 100, 'motorway_link': 60, 'secondary_link': 50,
+		# inferred
+		'road': 50, 'trunk_link': 60}
+	elif filename.startswith('beijing'):
+		# median speeds
+		speeds = {'secondary': 60, 'unclassified': 20,  'living_street': 25, 'primary': 60, 'service': 5,
+		'residential': 20, 'primary_link': 20, 'tertiary': 40, 'trunk': 80, 'motorway': 120, 'trunk_link': 40,
+		'motorway_link': 60,
+		# inferred
+		'road': 20, 'access_ramp': 40, 'track': 20,  'pedestrian': 20, 'tertiary_link': 30, 'secondary_link': 50}
+	return __linkNodes(ET.parse(filename).getroot(), speeds)
 
 #################
 # Find highways #
@@ -23,20 +38,21 @@ def buildRoadGraph(filename):
 # oneway, etc.
 # We keep all this information for future use
 
-def __linkNodes(root):
+def __linkNodes(root, speeds):
 	graph = nx.MultiDiGraph()
 	nodes = __findNodes(root)
 
 	edges = {}
-	noCars = ['footway','cycleway','path']
+	invalid = ['footway','cycleway','path', 'steps', 'construction', 'raceway',
+	'proposed', 'planned', 'bridleway', 'bus_stop', 'elevator', 'services',
+	'platform','disused', 'no', 'rest_area', 'crossing']
 	for way in root.findall('way'):
 		highway = False
 		for tag in way.findall('tag'):
-			if tag.get('k')=='highway' and tag.get('v') not in noCars:
+			if tag.get('k')=='highway' and tag.get('v') not in invalid:
 				highway	= True
-				break
 		if highway:
-			__addToGraph(way, nodes, edges, graph)
+			__addToGraph(way, nodes, edges, speeds, graph)
 	return (graph, edges)
 
 
@@ -62,24 +78,32 @@ def __findNodes(root):
 # directly storing their `id`. We store the rest of
 # the meta-data in a separate dict to conserve space.
 
-def __addToGraph(way, nodes, edges, graph):
+def __addToGraph(way, nodes, edges, speeds, graph):
 	edgeID = way.get('id')
 	edges[edgeID] = {tag.get('k'):tag.get('v') for tag in way.findall('tag')}
 	prevID = None
+	speed = speeds[edges[edgeID]['highway']]
+	if 'maxspeed' in edges[edgeID]:
+		speed = int(edges[edgeID]['maxspeed'])
+	elif 'maxspeed:lanes' in edges[edgeID]:
+		speed = int(edges[edgeID]['maxspeed:lanes'].split('|')[0])
+	edges[edgeID]['speed'] = speed
 	for node in way.findall('nd'):
 		nodeID = node.get('ref')
 		graph.add_node(nodeID, **nodes[nodeID])
 		if prevID:
 			dist = __distance(nodes[prevID]['loc'], nodes[nodeID]['loc'])
-			graph.add_edge(prevID, nodeID, key=edgeID, dist=dist)
+			weight = dist/speed
+			graph.add_edge(prevID, nodeID, key=edgeID, dist=dist, weight=weight)
 			if edges[edgeID].get('oneway', 'no') != 'yes':
-				graph.add_edge(nodeID, prevID, key=edgeID, dist=dist)
+				graph.add_edge(nodeID, prevID, key=edgeID, dist=dist, weight=weight)
 		prevID = nodeID
 
 
-# Distance formula (we should proably use haversine here)
+# Haversine
 def __distance(p1, p2):
-	return (p1[0]-p2[0])**2+(p1[1]-p2[1])**2
+	a = np.sin((p1[0]-p2[0])/2)**2+np.cos(p1[0])*np.cos(p2[0])*np.sin((p1[1]-p2[1])/2)**2
+	return 2*6371*np.arctan((a/(1-a))**1/2)
 
 
 # This is super computationally expensive, so watch out
@@ -93,6 +117,9 @@ def snapToGraph(trips, graph):
 		dists, indexes = NNTree.query(trips[tripID], k=1, n_jobs=-1)
 		maxError = max(max(dists), maxError)
 		snappedTrips[tripID] = []
+		j = -1
 		for i in indexes:
-			snappedTrips[tripID].append(nodeIDs[i])
+			if j!=i: #skip duplicates
+				snappedTrips[tripID].append(nodeIDs[i])
+			j = i
 	return (snappedTrips, maxError)
