187be1b53a9f3082b25f1145c3320c0b0fedac87
River <jkjk822@yahoo.com.au>
2018-05-03 16:57:03 -0400

    Write GEXF with pretty colors

--- portest.py
+++ portest.py
@@ -5,20 +5,45 @@ from streetMapParser import buildRoadGraph, snapToGraph
 from portugalTaxiParser import parseTrips
 #from beijingparse import parseTrips
 import numpy as np
-from graph import save_graph
 
 # Portugal Parse
 #trips = parseTrips('SamplePorto.txt')
 
-#graph, edgeTable = buildRoadGraph('portoMap.xml')
-#print("built")
-trips = {}
-with open("portoSnappedTrips.json", 'r') as f:
-	trips = json.loads(f.read())
+graph, edgeTable = buildRoadGraph('portoMap.xml')
+print("built")
+
+colors = [
+	{'color': {'r': 255, 'g': 102, 'b': 102, 'a': 0}},
+	{'color': {'r': 255, 'g': 0, 'b': 0, 'a': 0}},
+	{'color': {'r': 153, 'g': 76, 'b': 0, 'a': 0}},
+	{'color': {'r': 255, 'g': 128, 'b': 0, 'a': 0}},
+	{'color': {'r': 255, 'g': 255, 'b': 204, 'a': 0}},
+	{'color': {'r': 255, 'g': 255, 'b': 0, 'a': 0}},
+	{'color': {'r': 102, 'g': 102, 'b': 255, 'a': 0}},
+	{'color': {'r': 0, 'g': 0, 'b': 255, 'a': 0}},
+	{'color': {'r': 255, 'g': 153, 'b': 204, 'a': 0}},
+	{'color': {'r': 255, 'g': 0, 'b': 127, 'a': 0}},
+	{'color': {'r': 0, 'g': 153, 'b': 153, 'a': 0}},
+	{'color': {'r': 0, 'g': 255, 'b': 255, 'a': 0}},
+	{'color': {'r': 0, 'g': 102, 'b': 0, 'a': 0}},
+	{'color': {'r': 0, 'g': 255, 'b': 0, 'a': 0}},
+]
+with open("pathsToColor.json", 'r') as f:
+	count = 0
+	i = 0
+	for line in f:
+		if count%4 == 0:
+			print(line)
+		elif count%4 == 1 or count%4 == 2:
+			for node in json.loads(line):
+				graph.node[node]['viz'] = colors[i]
+				print(graph.node[node])
+			i+=1
+		count += 1
 print("loaded")
 
-nx.draw(trips)
-nx.write_gexf(trips, "porto.gexf")
+# nx.draw(trips)
+nx.write_gexf(graph, "porto.gexf")
 #save_graph(trips,"test.pdf")
 
 
